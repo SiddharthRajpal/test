85cf286131dbce3555d76641adf61527f01c4688
SiddharthRajpal <77538244+SiddharthRajpal@users.noreply.github.com>
2021-01-19 11:24:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>login to access database</title>
+
+</head>
+<body>
+  <style>
+     .button {
+      float: left;
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;}</style>
+  <form action="cookie.php", method="GET">
+    <h3>Login to Access database</h3>
+    <br>  
+    <input type="text" class="form-control" placeholder="Enter Username" name="user">
+    <br>
+    <br>
+    <input type="text" class="form-control" name="Password" placeholder="Enter Password">
+    <br>
+    <button type="submit" class="button button1">Continue</button>
+
+
+</body>
+</html>
